65154a41da3830529f5544617716eb2d8f85e59c
Zarihun Bayisa <zarihunbayisa76@gmail.com>
2025-03-09 23:57:18 -0700

    Initial commit - Upload project files

---
+++ Desktop/SchoolManagement-CI/application/views/backend/footer.php
@@ -0,0 +1,5 @@
+<!-- Footer -->
+<footer class="main">
+	&copy; <?php echo date('Y')?> | School Management System in PHP using CodeIgniter Web Framework |
+    <strong>Developed by Md Riadul Islam, TMSS ICT</strong>
+</footer>
